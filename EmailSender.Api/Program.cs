f64867855fcf846a6c498d8453c94de49d02f7ec
Nikita Mokhnachev <fu.manchouz@gmail.com>
2022-02-04 13:24:46 +0300

    Configure SMTP

--- EmailSender.Api/Program.cs
+++ EmailSender.Api/Program.cs
@@ -1,3 +1,10 @@
+using EmailSender.Api.Data.Models;
+using EmailSender.Api.Smtp;
+
+using MailKit.Net.Smtp;
+
+using MimeKit;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var config = builder.Configuration;
@@ -6,6 +13,35 @@ var config = builder.Configuration;
 builder.Services.AddRouting(options =>
 {
     options.LowercaseUrls = true;
+});
+
+// Configure SMTP
+builder.Services.AddTransient<SmtpClient>();
+
+builder.Services.AddTransient<SmtpSession>(provider =>
+{
+    var client = provider.GetService<SmtpClient>() ?? throw new NullReferenceException();
+
+    var server = new SmtpServerSettings(
+        config["Smtp:Server:Host"],
+        int.Parse(config["Smtp:Server:Port"]),
+        bool.Parse(config["Smtp:Server:UseSsl"]));
+
+    var name = config.GetValue<string>("Name");
+    var address = config.GetValue<string>("Address");
+
+    if (config["Credentials:Login"] is not null)
+    {
+        var credentials = new UserCredentials
+        {
+            Login = config["Credentials:Login"],
+            Password = config["Credentials:Password"]
+        };
+
+        return new AuthorizedSmtpSession(client, server, new MailboxAddress(name, address), credentials);
+    }
+
+    return new SmtpSession(client, server, new MailboxAddress(name, address));
 });
 
 // Configure controllers
